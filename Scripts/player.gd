5802cb49641b46d435c7d63ad6134cd827abedcf
Austin Dorriety <ardorriety@gmail.com>
2024-07-02 01:05:24 -0400

    Raycasts added and debug attempts

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,7 @@ var game: Node
 var player: AnimatedSprite2D
 var direction: float = 1
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var raycast: RayCast2D
 #Stats----------------------------------------------------------------------------------------------
 var health: int
 var defense: int
@@ -12,7 +13,7 @@ var defense: int
 var speed: int = 220
 var movement: bool = false
 #Jump-----------------------------------------------------------------------------------------------
-var force: int = -200
+var force: int = -600
 var jumped: bool = false
 #Blink----------------------------------------------------------------------------------------------
 var potency: int = 275
@@ -25,6 +26,7 @@ func _ready():
 	game = get_node("/root/Gamemanager")
 	player = $AnimSprite
 	blinkcooldown = $Blink
+	raycast = $RayCast2D
 func _process(_delta):
 	pass
 func _physics_process(delta):
@@ -38,25 +40,31 @@ func _physics_process(delta):
 func playerjump():
 	if Input.is_action_just_pressed("Up") and jumped == false:
 		jumped = true
-		position.y = global_position.y + force
+		raycast.target_position.y = force
+		raycast.target_position.x = 0
+		print(raycast.get_collider())
 		#jump anim track here
 	elif is_on_floor():
 		jumped = false
+		raycast.target_position.y = 0
 #set player direction-------------------------------------------------------------------------------`
 func setdirection():
 	if Input.is_action_pressed("Right"):
 		direction = 1
+		raycast.target_position.x = potency * direction
 		velocity.x = direction * speed
 		player.flip_h = false
 		#movement anim track here
 		movement = true
 	elif Input.is_action_pressed("Left"):
 		direction = -1
+		raycast.target_position.x = potency * direction
 		velocity.x = direction * speed
 		player.flip_h = true
 		#movement anim track here
 		movement = true
 	else:
+		raycast.target_position.x = potency * direction * -1
 		velocity.x = move_toward(velocity.x, 0, speed)
 		movement = false
 #set player blink-----------------------------------------------------------------------------------
@@ -64,8 +72,14 @@ func blink():
 	if Input.is_action_just_pressed("Blink") and blinked == false:
 		currentcharge += 1
 		if currentcharge >= blinkcharges: blinked = true
-		if movement == false: position.x = global_position.x + (potency * direction) * -1
-		else: position.x = global_position.x + (potency * direction)
+		if movement == false:
+			if raycast.is_colliding(): 
+				potency = position.x - raycast.get_collider().position.x * -1
+			else: potency = 275
+			position.x += potency * direction * -1
+		else: 
+			if raycast.is_colliding(): position.x = raycast.get_collider().position.x
+			else: position.x = global_position.x + (potency * direction)
 		print(potency * direction)
 		print(currentcharge)
 		blinkcooldown.start()
