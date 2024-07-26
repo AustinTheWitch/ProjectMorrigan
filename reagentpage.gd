53d5b4a73931a57b420b8016342c35c89785f819
Austin Dorriety <ardorriety@gmail.com>
2024-07-26 00:12:16 -0400

    Set up Reagent Page Basics

--- reagentpage.gd
+++ reagentpage.gd
@@ -1,11 +1,16 @@
 extends Control
 
+@onready var page: VBoxContainer = $Page
+@onready var reagent_name: Label = %Name
+@onready var sprite: TextureRect = %Sprite
+@onready var primary: Label = %Primary
+@onready var secondary: Label = %Secondary
+@onready var tertiary: Label = %Tertiary
+@onready var description: RichTextLabel = %Description
+var reagent: Reagent
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float):
 	pass
