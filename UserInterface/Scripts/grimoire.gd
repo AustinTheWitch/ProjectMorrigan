8d52d7fe5e96c40f7d350b430e1a3ce1edf2c980
Austin Dorriety <ardorriety@gmail.com>
2024-07-29 23:21:47 -0400

    Figuring Grimoire Layout

--- UserInterface/Scripts/grimoire.gd
+++ UserInterface/Scripts/grimoire.gd
@@ -1,15 +1,18 @@
 extends CanvasLayer
 
 const FAMILIAR_PAGE = preload("res://UserInterface/familiarpage.tscn")
-@onready var familar_section: TabContainer = $AspectRatioContainer/Binding/Ch_Familiar
+@onready var familiar_section: TabContainer = $Background/Ch_Familiar
 const Familiarpage = preload("res://UserInterface/Scripts/familiarpage.gd")
 
 const REAGENT_PAGE = preload("res://UserInterface/reagentpage.tscn")
-@onready var reagent_section: TabContainer = $AspectRatioContainer/Binding/Ch_Reagent
+@onready var reagent_section: TabContainer = $Background/Ch_Reagent
 const Reagentpage = preload("res://UserInterface/Scripts/reagentpage.gd")
 
 func _ready():
-	load_familiars()
+	#load_familiars()
+	#reagent_index()
+	#page_structure()
+	pass
 func _process(_delta: float):
 	pass
 func load_familiars():
@@ -17,11 +20,11 @@ func load_familiars():
 		var page : Familiarpage = FAMILIAR_PAGE.instantiate() as Familiarpage
 		page.familiar = familiar
 		page.name = familiar.familiar_name
-		familar_section.add_child(page)
-		self.visible = false
+		familiar_section.add_child(page)
 
 func reagent_index():
 	for reagent : Reagent in Gamemanager.reagentlist:
 		var page : Reagentpage = REAGENT_PAGE.instantiate() as Reagentpage
 		page.reagent = reagent
 		page.name = reagent.reagent_name
+		reagent_section.add_child(page)
